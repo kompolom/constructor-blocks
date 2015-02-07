6f6b598068ce937e1aa03942dcfc1bf15338ceb2
Evgeniy Baranov <master@kompolom.ru>
2015-02-07 11:23:52 +0300

    add form bh.php template

---
+++ form/form.bh.php
@@ -0,0 +1,14 @@
+<?php
+return function ($bh) {
+ $bh->match("form", function ($ctx, $json){
+   $ctx
+     ->tag('form')
+     ->attrs([
+       'action'=>$ctx->param('action'),
+       'id'=>$ctx->param('id'),
+       'method'=>$json->method?
+         $ctx->param('method') :
+         'get'
+       ]);
+ });
+};
