efd101ee281eed707e5e1859deeae212e907436f
Evgeniy Baranov <kompolom@yandex.ru>
2017-06-11 22:16:16 +0300

    Delete section.bh.js

--- section.bh.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (bh) {
- bh.match("section", function (ctx, json) {
-   ctx.tag('section')
-     .attr('id', 'section-' + ctx.param('name') + '-' + ctx.position())
-     .mod('name', ctx.param('name'))
-     .applyBase();
- }
-);
-};
